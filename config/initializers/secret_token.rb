928b5a0b823afd68f2303b66e257602364f64e7f
Michel Belleville <michel.belleville@gmail.com>
2011-02-23 09:57:48 +0100

    Basic Ruby on Rails app using sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioViny::Application.config.secret_token = '8f534c8f214315b6f95367d7069144c00419f257ad991aef7c9d715c711cf462461ec8c5ee43ffbd6f054be70fc32eea391d4b98c08fc9eca96faa5ec6888828'
