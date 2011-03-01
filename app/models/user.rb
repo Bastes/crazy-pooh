5b9d8167c4503ec5f918cd823276b768161f3179
Michel Belleville <michel.belleville@gmail.com>
2011-03-01 16:14:44 +0100

    Validations for User and locales

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :login, :presence => true, :uniqueness => true, :length => { :minimum => 1 }
+  validates :password, :presence => true, :length => { :minimum => 8 }
 end
