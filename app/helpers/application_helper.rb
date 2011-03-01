039c5ead8372b061630de60e75888e6962bf8ad7
Michel Belleville <michel.belleville@gmail.com>
2011-03-01 16:53:57 +0100

    ApplicationHelper#errors_on

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def errors_on(model, field = nil)
+    errors = field.nil? ? model.errors.full_messages : model.errors[field]
+    if errors.any?
+      errors.map { |error|
+        content_tag 'div', error, :class => 'error'
+      }.join("\n").html_safe
+    end
+  end
 end
