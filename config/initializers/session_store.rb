acefc8f864104c2ef4fc4410fd9b6b240bdd3400
Michel Belleville <michel.belleville@gmail.com>
2011-03-01 16:15:52 +0100

    Sessions stored in-base

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-PortfolioViny::Application.config.session_store :cookie_store, :key => '_portfolio-viny_session'
+PortfolioViny::Application.config.session_store :active_record_store, :key => '_portfolio-viny_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
