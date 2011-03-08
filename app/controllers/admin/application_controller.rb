db7b3c0946e09358323259701aff94a13cce5fa7
Michel Belleville <michel.belleville@gmail.com>
2011-03-08 15:11:34 +0100

    Factoring user and admin? methods to ApplicationController

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,21 +1,9 @@
-class Admin::ApplicationController < ActionController::Base
-  protect_from_forgery
-
+class Admin::ApplicationController < ApplicationController
   layout 'admin'
 
   before_filter :restricted
 
   protected
-
-  def admin?
-    user && true
-  end
-  helper_method :admin?
-
-  def user
-    @user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :user
 
   def restricted
     Rails.logger.debug "session: #{session.inspect}"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  def admin?
+    user && true
+  end
+  helper_method :admin?
+
+  def user
+    @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :user
 end
